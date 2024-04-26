ca7477b19066f81bff2bd92e69893b01f1b80a5e
vidushankalj <vidulakshanlj@gmail.com>
2024-04-26 14:33:45 +0530

    models & schemas

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         // listen for rquests
         app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
+    console.log('connected to db & listening on port', process.env.PORT);
     })
     })
     .catch((error) => {
