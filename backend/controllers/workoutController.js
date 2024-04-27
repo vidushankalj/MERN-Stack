fc6641ec71b2083eb18e160052df13111a5801c7
vidushankalj <vidulakshanlj@gmail.com>
2024-04-28 03:56:59 +0530

    error handling backend

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -28,6 +28,21 @@ const getWorkout = async (req, res) => {
 // create new workout
 const createWorkout = async (req, res) => {
     const {title, reps, load} = req.body
+
+    let emptyFields = []
+
+    if (!title) {
+        emptyFields.push('title')
+    }
+    if (!load) {
+        emptyFields.push('load')
+    }
+    if (!reps) {
+        emptyFields.push('reps')
+    }
+    if (emptyFields.length > 0) {
+        return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
+    }
     
     // add doc to db
     try{
